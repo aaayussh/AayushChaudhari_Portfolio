056d75ed08048d10cfb8f70144d0d820a9db95eb
Aayush Chaudhari <80114786+aaayussh@users.noreply.github.com>
2024-02-02 05:32:47 +0530

    Update NavBar.jsx

--- src/componets/NavBar.jsx
+++ src/componets/NavBar.jsx
@@ -33,7 +33,7 @@ const NavBar = () => {
   return (
     <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed">
       <div>
-        <h1 className="text-5xl font-signature ml-2">Aayush Chuadhari</h1>
+        <h1 className="text-5xl font-signature ml-2">Aayush Chaudhari</h1>
       </div>
 
       <ul className="hidden md:flex">
